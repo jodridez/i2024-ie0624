2f1bfb704fa02510a25172d1b633ee0b93295405
jodridez <jodridez.2.0@gmail.com>
2024-04-15 18:17:31 -0600

    Se mejora el estado de advertencia_vehiculos

--- Laboratorio2/src/cruce_semaforos.c
+++ Laboratorio2/src/cruce_semaforos.c
@@ -23,6 +23,7 @@ Descripcion:
 //Variables;
 int press_boton = 0;
 int medio_segundo = 0;
+int contador = 0;
 
 // Atencion de interrupciones
 ISR(INT0_vect) {
@@ -60,7 +61,7 @@ void configurarInterrupciones() {
 
    // Configura Timer/Counter1 en modo CTC (Clear Timer on Compare Match)
     TCCR1B |= (1 << WGM12); // Modo CTC
-    OCR1A = 3906; // Valor de comparación para generar una interrupción cada 0.5 segundos (FCPU = 1MHz, prescaler = 1024)
+    OCR1A = 976; // Valor de comparación para generar una interrupción cada 0.5 segundos (FCPU = 1MHz, prescaler = 1024)
     // Configura el prescaler a 1024
     TCCR1B |= (1 << CS12) | (1 << CS10); //prescaler = 1024
     // Habilita la interrupción por Comparación de salida de Timer/Counter1 (Output Compare Match A)
@@ -76,6 +77,7 @@ int main() {
     estado = PASO_VEHICULOS;
     press_boton = 0;
     medio_segundo = 0;
+    contador = 0;
 
     while (1) { 
         FMS();
@@ -91,21 +93,22 @@ void FMS(){
             // LDPV y LDPD encendido, los demas apagados
             PORTB |= 0b0001001;
             if(press_boton){
-                PORTB &= 0b11110000;
+                PORTB &= 0b11110000; //Apaga todos los LEDs
                 estado = ADVERTENCIA_VEHICULOS;
                 press_boton = 0;
             }
         break;
 
         case ADVERTENCIA_VEHICULOS:
-            if(medio_segundo%2==0){
-                PORTB = (1<<PB0);
+            //Enciende LDPD y parpadea LDPV
+            PORTB |= (1<<PB3); // Se mantiene encendido
+            if (medio_segundo % 2 == 0) {
+                PORTB ^= (1 << PB0); // Invierte el estado del pin 0
             }
-            else{
-                PORTB = (0<<PB0);
-            }
-            estado = ADVERTENCIA_VEHICULOS;
+        break;
 
+        case DETENCION_VEHICULOS:
+            //Enciende LDVD y LDPP
         break;
 
         default:
